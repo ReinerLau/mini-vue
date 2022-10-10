a040ad4451da7f4c860ec07fd63a85f94ece702d
reiner850593913 <lk850593913@gmail.com>
2022-10-10 21:57:24 +0800

    feat(reactivity): 实现 isRef 和 unRef 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,7 +6,7 @@ import { reactive } from "./reactive";
  * @Author: reiner850593913 lk850593913@gmail.com
  * @Date: 2022-10-09 22:28:56
  * @LastEditors: reiner850593913 lk850593913@gmail.com
- * @LastEditTime: 2022-10-10 20:35:24
+ * @LastEditTime: 2022-10-10 21:53:58
  * @FilePath: \mini-vue\src\reactivity\ref.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,6 +14,7 @@ class RefImpl {
   private _value: any;
   private dep = new Set();
   private _rawValue: any;
+  public __v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     this._value = convert(value);
@@ -45,4 +46,12 @@ function trackRefValue(dep) {
 
 function convert(value) {
   return isObject(value) ? reactive(value) : value;
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -2,13 +2,14 @@
  * @Author: reiner850593913 lk850593913@gmail.com
  * @Date: 2022-10-09 22:27:13
  * @LastEditors: reiner850593913 lk850593913@gmail.com
- * @LastEditTime: 2022-10-10 20:22:11
+ * @LastEditTime: 2022-10-10 21:54:23
  * @FilePath: \mini-vue\src\reactivity\tests\ref.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, ref, unRef } from "../ref";
 
 describe("ref", () => {
   it("happy path", () => {
@@ -46,5 +47,22 @@ describe("ref", () => {
     expect(dummy).toBe(1);
     foo.value.count++;
     expect(dummy).toBe(2);
+  });
+
+  it("isRef", () => {
+    const foo = ref(1);
+    const user = reactive({
+      foo: 1,
+    });
+
+    expect(isRef(foo)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
+
+  it("unRef", () => {
+    const foo = ref(1);
+    expect(unRef(foo)).toBe(1);
+    expect(unRef(1)).toBe(1);
   });
 });
