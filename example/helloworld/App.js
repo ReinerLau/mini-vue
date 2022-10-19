14d172d6d223f3b1d4f91e5e6dd37158b1dfd1fe
reiner850593913 <lk850593913@gmail.com>
2022-10-19 18:51:07 +0800

    feat(runtime): 实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,7 @@
  * @Author: reiner850593913 lk850593913@gmail.com
  * @Date: 2022-10-15 10:10:18
  * @LastEditors: reiner850593913 lk850593913@gmail.com
- * @LastEditTime: 2022-10-17 21:26:05
+ * @LastEditTime: 2022-10-18 21:51:17
  * @FilePath: \mini-vue\example\helloworld\App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,10 @@ import { h } from "../../lib/mini-vue.esm.js";
 
 export const App = {
   render() {
-    return h("div", "hi, " + this.msg);
+    return h("div", { id: "root", class: ["red", "hard"] }, [
+      h("div", { class: "blue" }, "hi"),
+      h("div", { class: "red" }, "mini-vue"),
+    ]);
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,10 +1,11 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 /*
  * @Author: reiner850593913 lk850593913@gmail.com
  * @Date: 2022-10-15 10:44:19
  * @LastEditors: reiner850593913 lk850593913@gmail.com
- * @LastEditTime: 2022-10-17 21:42:56
+ * @LastEditTime: 2022-10-18 21:57:07
  * @FilePath: \mini-vue\src\runtime-core\renderer.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +14,40 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const el = document.createElement(vnode.type);
+
+  const { children } = vnode;
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildern(vnode, el);
+  }
+
+  const { props } = vnode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildern(vnode, el) {
+  vnode.children.forEach((v) => {
+    patch(v, el);
+  });
 }
 
 function processComponent(vnode, container) {
